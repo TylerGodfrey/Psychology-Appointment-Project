7488d3c18a2a1fe1cd002ec7a356cb11fe27ac91
LAPTOP-4IMGE7TU\yerfd <tyler.godfrey@my.simpson.edu>
2017-11-07 12:54:20 -0600

    Created calendar class with separate functions for the top and bottom halves so that subclasses could redefine the bottom half without duplicating the code for the top half.

---
+++ calendar_class.php
@@ -0,0 +1,97 @@
+<html>
+<head>
+</head>
+<body>
+
+<?php
+class Calendar {
+private $monthNames = Array("January", "February", "March", "April", "May", "June", "July", 
+"August", "September", "October", "November", "December");
+
+function createTopOfCalendar () {
+	if (!isset($_REQUEST["month"])) $_REQUEST["month"] = date("n");
+	if (!isset($_REQUEST["year"])) $_REQUEST["year"] = date("Y");
+
+	$this->cMonth = $_REQUEST["month"];
+	$this->cYear = $_REQUEST["year"];
+	 
+	$prev_year = $this->cYear;
+	$next_year = $this->cYear;
+	$prev_month = $this->cMonth-1;
+	$next_month = $this->cMonth+1;
+	 
+	if ($prev_month == 0 ) {
+	    $prev_month = 12;
+	    $prev_year = $this->cYear - 1;
+	}
+	if ($next_month == 13 ) {
+	    $next_month = 1;
+	    $next_year = $this->cYear + 1;
+	}
+
+	echo "
+
+	<table width='1200'>
+	<tr align='center'>
+	<td bgcolor='#999999' style='color:#FFFFFF'>
+	<table width='100%' border='0' cellspacing='0' cellpadding='0'>
+	<tr>
+	<td width='50%' align='left'>  <a href='" . $_SERVER['PHP_SELF'] . '?month='. $prev_month . '&year=' . $prev_year . "' style='color:#FFFFFF'>Previous</a></td>
+	<td width='50%' align='right'><a href='" . $_SERVER['PHP_SELF'] . '?month='. $next_month . '&year=' . $next_year . "' style='color:#FFFFFF'>Next</a>  </td>
+	</tr>
+	</table>
+	</td>
+	</tr>
+	<tr>
+	<td align='center'>
+	<table width='100%' border='0' cellpadding='2' cellspacing='2'>
+	<tr align='center'>
+	<td colspan='7' bgcolor='#999999' style='color:#FFFFFF'><strong>" . $this->monthNames[$this->cMonth-1].' '.$this->cYear . "</strong></td>
+	</tr>
+	<tr>
+	<td align='center' bgcolor='#999999' style='color:#FFFFFF'><strong>Sunday</strong></td>
+	<td align='center' bgcolor='#999999' style='color:#FFFFFF'><strong>Monday</strong></td>
+	<td align='center' bgcolor='#999999' style='color:#FFFFFF'><strong>Tuesday</strong></td>
+	<td align='center' bgcolor='#999999' style='color:#FFFFFF'><strong>Wednesday</strong></td>
+	<td align='center' bgcolor='#999999' style='color:#FFFFFF'><strong>Thursday</strong></td>
+	<td align='center' bgcolor='#999999' style='color:#FFFFFF'><strong>Friday</strong></td>
+	<td align='center' bgcolor='#999999' style='color:#FFFFFF'><strong>Saturday</strong></td>
+	</tr>
+	";
+}
+
+function createBottomOfCalendar () {
+
+	$timestamp = mktime(0,0,0,$this->cMonth,1,$this->cYear);
+	$maxday = date("t",$timestamp);
+	$thismonth = getdate ($timestamp);
+	$startday = $thismonth['wday'];
+	for ($i=0; $i<($maxday+$startday); $i++) {
+	    if(($i % 7) == 0 ) echo "<tr>";
+	    if($i < $startday) echo "<td></td>";
+	    else {
+	    	$date = date_create(strval($this->cYear) . '-' . strval($thismonth['mon']) . '-' . strval($i - $startday + 1));
+	    	echo "<td align='left' valign='top' width='110px' height='110px'>". ($i - $startday + 1) .
+	    	 "</td>";
+	    }
+	    if(($i % 7) == 6 ) echo "</tr>";
+	}
+
+	echo "
+	</table>
+	</td>
+	</tr>
+	</table>
+	";
+	}
+}
+?>
+
+<?php
+$baseCalendar = new Calendar();
+$baseCalendar->createTopOfCalendar();
+$baseCalendar->createBottomOfCalendar();
+?>
+
+</body>
+</html>
