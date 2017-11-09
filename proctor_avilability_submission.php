c7ccebc5cf068ed9cd43e160686320e9dcc6f60f
Cole Holland <cole.holland@my.simpson.edu>
2017-11-09 16:54:17 -0600

    Added functionality needed to submit to database.

--- proctor_avilability_submission.php
+++ proctor_avilability_submission.php
@@ -4,8 +4,16 @@
  </head>
 <body>
 <?php
-?procSub
 include ("general_connection.php");
+
+$procSub = new Connection();
+$procSub->tableName = "experimenter availability";
+$procSub->columnNames = ['StudyID', 'ExperimenterID', 'Date', 'StartTime',    															'EndTime'];
+$procSub->getValues();
+$procSub->createStatement();
+$procSub->createConnection();
+$procSub->submit();
+$procSub->killConnection();
 
 ?>
 </body>
