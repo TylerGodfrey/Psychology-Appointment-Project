52fe8d595466d50e9ac51ca7cfee99d9202351c3
Cole Holland <cole.holland@my.simpson.edu>
2017-12-05 16:42:04 -0600

    Links for the menu on each page.

---
+++ links.php
@@ -0,0 +1,7 @@
+<?php
+echo "<a href='index.php'>Home</a> -
+	<a href='refactoring_calendar.php'>Proctor Availability Sign-Up</a> -
+	<a href='appointment_creation.php'>Appointment Creation</a> -
+	<a href='study_selection.php'>Study Selection</a> -
+	<a href='calendar_class.php'>Student Study Time Sign-Up</a>";
+?>
